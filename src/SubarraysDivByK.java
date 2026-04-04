4afa36817bc5dc034ab2469f265e6717e835919e
Asabhiroop20 <abhiroopas2423@gmail.com>
2026-04-04 11:44:35 +0530

    first update of subarrays program

--- src/SubarraysDivByK.java
+++ src/SubarraysDivByK.java
@@ -1,7 +1,42 @@
+import java.util.*;
 public class SubarraysDivByK {
     
-        // TODO: Read n and k
-        // TODO: Read array elements
-        // TODO: Count and print the number of subarrays with sum divisible by k
-    
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // important
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            prefixSum += nums[i];
+
+            int rem = prefixSum % k;
+
+            // handle negative remainder
+            if (rem < 0) {
+                rem += k;
+            }
+
+            if (map.containsKey(rem)) {
+                count += map.get(rem);
+            }
+
+            map.put(rem, map.getOrDefault(rem, 0) + 1);
+        }
+
+        System.out.println(count);
+    }
 }
